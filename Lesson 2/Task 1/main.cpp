af99857bb938cdc4832772db5011f8e98d61b4a6
Alexandr Sosna <50953271+AlexandrSosna@users.noreply.github.com>
2021-10-27 18:44:27 +0300

    Complete task 1 for lesson 2

---
+++ Lesson 2/Task 1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+	int result;
+    cout << "Enter number: ";
+    cin >> number;
+    for(int i = number; i>0; i/=10)
+	{
+			result +=i%10;
+	}
+    cout << "The sum of the number: " << result << endl;
+    return 0;
+}

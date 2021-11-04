21593400a7a3f4c12957010d513a83b2f63784a4
Alexandr Sosna <50953271+AlexandrSosna@users.noreply.github.com>
2021-11-04 19:26:32 +0200

    fixed code style

--- Lesson 2/Task 1/main.cpp
+++ Lesson 2/Task 1/main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main()
 {
     int number;
-	int result;
+    int result;
     cout << "Enter number: ";
     cin >> number;
     for(int i = number; i>0; i/=10)
 	{
-			result +=i%10;
+        result +=i%10;
 	}
     cout << "The sum of the number: " << result << endl;
     return 0;

31cef7942a25a4534a5bc0f94cfaec830fdd7887
Alexandr Sosna <50953271+AlexandrSosna@users.noreply.github.com>
2021-11-14 17:53:00 +0200

    fixed something...

--- Lesson 2/Task 1/main.cpp
+++ Lesson 2/Task 1/main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main()
 {
-    int number,result;
+    int number = 0;
+    int result = 0;
     cout << "Enter number: ";
     cin >> number;
-    for(int i = number; i>0; i/=10){
-        result +=i%10;
-	}
+    for(int i=number; i>0; i/=10){
+      result +=i%10;
+  }
     cout << "The sum of the number: " << result << endl;
     return 0;
 }
